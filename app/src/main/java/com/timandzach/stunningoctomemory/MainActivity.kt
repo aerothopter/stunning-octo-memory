0bcd89a9506ae120a1c9b759f323b8ed4c895d69
Zach Cameron <zach@learnspeed.com>
2019-08-20 19:29:24 -0700

    Stuff to support Maps activity

--- app/src/main/java/com/timandzach/stunningoctomemory/MainActivity.kt
+++ app/src/main/java/com/timandzach/stunningoctomemory/MainActivity.kt
@@ -7,6 +7,9 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+//        val sharedPref = getSharedPreferences(
+//
+//        )
         setContentView(R.layout.activity_main)
     }
 }
--- app/src/main/java/com/timandzach/stunningoctomemory/control/SpeedListener.kt
+++ app/src/main/java/com/timandzach/stunningoctomemory/control/SpeedListener.kt
@@ -3,5 +3,5 @@ SpeedListener registers with a SpeedNotifier
  */
 
 interface SpeedListener {
-    fun updateSpeed(speed : Double)
+    fun updateSpeed(latitude: Double, longitude: Double)
 }
