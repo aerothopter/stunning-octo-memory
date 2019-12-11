e28d33958c13d3a9b9b887a873aa5f6238dba9eb
Zach Cameron <zach@learnspeed.com>
2019-12-10 20:22:19 -0800

    Update finish() method to quit everything

--- app/src/main/java/com/timandzach/stunningoctomemory/DebugActivity.kt
+++ app/src/main/java/com/timandzach/stunningoctomemory/DebugActivity.kt
@@ -1,11 +1,5 @@
 package com.timandzach.stunningoctomemory
 
-import android.os.Bundle
-import java.util.Formatter
-import java.util.Locale
-import android.Manifest
-import android.annotation.SuppressLint
-import android.content.pm.PackageManager
 import android.app.Activity
 import android.content.Context
 import android.content.SharedPreferences
@@ -14,11 +8,12 @@ import android.widget.CheckBox
 import android.widget.CompoundButton
 import android.widget.CompoundButton.OnCheckedChangeListener
 import android.content.Intent
+import android.content.SharedPreferences
+import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
+import java.util.*
 
 
 class DebugActivity : Activity(), SpeedListener {
@@ -71,14 +66,10 @@ class DebugActivity : Activity(), SpeedListener {
     override fun finish() {
         super.finish()
 
-        val serviceIntent = Intent(this, LocationService::class.java)
-        stopService(serviceIntent)
-
-        service_running = false
-
-        speedNotifier.unregister(this)
-
-        System.exit(0)
+        val intent = Intent(this, MapsActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+        intent.putExtra("EXIT", true)
+        startActivity(intent)
     }
 
     override fun updateLatLong(latitude: Double, longitude: Double) {
